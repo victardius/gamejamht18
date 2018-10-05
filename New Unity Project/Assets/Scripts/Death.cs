3a27f015e86f4a29dfe4ed9dec8a9c5b3845f2cd
leas9594 <leonard.asplund@gmail.com>
2018-10-05 23:09:31 +0200

    LavaLevel

---
+++ New Unity Project/Assets/Scripts/Death.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Death : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+       Scene loadedLevel = SceneManager.GetActiveScene();
+       SceneManager.LoadScene(loadedLevel.buildIndex);
+
+    }
+    
+
+
+
+}
---
+++ New Unity Project/Assets/Scripts/End.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class End : MonoBehaviour {
+
+    bool enterd = false;
+    public float timeLeft = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (enterd)
+        {
+            timeLeft -= Time.deltaTime;
+        }
+        
+
+        if (timeLeft <= 0)
+        {
+            Debug.Log("Enter new sceane");
+            SetSphere(true);
+            
+            timeLeft = 5.0f;
+            enterd = false;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        enterd = true;
+        Debug.Log("Collect code");
+        SetSphere(false);
+
+        
+    }
+
+    private void SetSphere(bool state)
+    {
+        
+        if (gameObject.GetComponent<SphereCollider>())
+        {
+            gameObject.GetComponent<SphereCollider>().enabled = state;
+        }
+        else if (gameObject.GetComponent<BoxCollider>())
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = state;
+        }
+        gameObject.GetComponent<MeshRenderer>().enabled = state;
+    }
+
+}
+
