625be658411bfc422cee46bf9b9e036a05390a8d
victardius <37775040+victardius@users.noreply.github.com>
2018-10-05 22:18:02 +0200

    ages

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -4,24 +4,33 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed, initialSpeed = 2, maxSpeed = 10, rotationSpeed = 120;
+    public float speed, initialSpeed = 2, maxSpeed = 25, rotationSpeed = 120, jump = 10;
+    public GameObject groundCheck;
 
-    private float xRotation, zMovement;
+    private float xRotation, zMovement, groundDistance;
+    private Rigidbody rgbd;
 
     void Start () {
+        rgbd = GetComponent<Rigidbody>();
         speed = initialSpeed;
+        //groundDistance = BoxCollider.
 	}
 	
 	void Update ()
     {
-        xRotation = Input.GetAxis("Horizontal") * Time.deltaTime * rotationSpeed;
+        xRotation = Input.GetAxis("Horizontal") * Time.deltaTime * (rotationSpeed + speed * 5);
         zMovement = Time.deltaTime * speed;
         transform.Rotate(0, xRotation, 0);
         transform.Translate(0, 0, zMovement);
         if (speed<maxSpeed)
-            speed = speed + 25 * Time.deltaTime;
+            speed = speed + 1 * Time.deltaTime;
 
-        //Input.GetAxis("Vertical") * Time.deltaTime * rotationSpeed;
+        
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            rgbd.AddForce(transform.up * jump);
+        }
 
 	}
 }
