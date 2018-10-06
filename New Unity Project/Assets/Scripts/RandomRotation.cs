3ea724e9328d2b47145b8d2b3382c778d049972d
leas9594 <38495831+leas9594@users.noreply.github.com>
2018-10-06 21:26:44 +0200

    Mechs

---
+++ New Unity Project/Assets/Scripts/RandomRotation.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomRotation : MonoBehaviour {
+
+	void Start () {
+        int rndNumber = Random.Range(0, 361);
+        transform.eulerAngles = new Vector3(0, rndNumber, 0);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
