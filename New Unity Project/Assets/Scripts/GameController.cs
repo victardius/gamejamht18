e97dc0b5b7c1ef2b4b41a8a8a6b7e3e719efe263
victardius <37775040+victardius@users.noreply.github.com>
2018-10-06 20:39:49 +0200

    aeswg

---
+++ New Unity Project/Assets/Scripts/GameController.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+    public static int orbsCollected;
+    public static int deaths;
+
+	void Start () {
+        orbsCollected = 0;
+	}
+}
