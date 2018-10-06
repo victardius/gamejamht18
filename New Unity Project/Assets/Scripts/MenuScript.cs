e2e6f8cc5737381d07d0a83370d5c6b0d56a9905
Hjallimar <hjalle@ownit.nu>
2018-10-06 14:35:37 +0200

    weee

---
+++ New Unity Project/Assets/Scripts/MenuScript.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuScript : MonoBehaviour {
+
+
+    public GameObject quitPanel;
+    public GameObject rektPanel;
+    public GameObject lorePanel;
+    public GameObject quitbutton;
+
+    // Use this for initialization
+    void Start () {
+        quitPanel.SetActive(false);
+        rektPanel.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void toggleQuitPanel()
+    {
+        quitPanel.SetActive(true);
+        quitbutton.SetActive(false);
+    }
+
+    public void toggleRektPanel()
+    {
+        quitPanel.SetActive(false);
+        rektPanel.SetActive(true);
+    }
+
+    public void backToMenu()
+    {
+        quitbutton.SetActive(true);
+        quitPanel.SetActive(false);
+        rektPanel.SetActive(false);
+    }
+}
