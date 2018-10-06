610bd0c92c10dfdd6c59f730d37359fece1a0962
Hjallimar <hjalle@ownit.nu>
2018-10-06 22:08:36 +0200

    fuck meee

---
+++ New Unity Project/Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour {
+
+    // Use this for initialization
+    public Text uiText;
+    private List<string> names;
+    private Scene currentScene;
+
+    void Start () {
+        names.Add("Main Menu");
+        names.Add("Drag Race");
+        names.Add("Maze Daze");
+        names.Add("Lava Town");
+        names.Add("Wood Land of Doom");
+        names.Add("Desert Dessert");
+        names.Add("Disco Dance");
+
+        currentScene = SceneManager.GetActiveScene();
+        int nextLvl = currentScene.buildIndex + 1;
+        uiText.text = names[nextLvl];
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
